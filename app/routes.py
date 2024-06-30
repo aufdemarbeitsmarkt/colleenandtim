c11fd3921f409e053941feabde8a1e333cdf2941
Tim <37612712+aufdemarbeitsmarkt@users.noreply.github.com>
2024-06-30 16:20:36 -0700

    add get method to index

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask import flash, redirect, render_template, url_for
 from app import app
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html', title='Colleen & Tim')
