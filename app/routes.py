a0cc0daec01558eda531cc8d62c8fbeac40052a5
Tim <37612712+aufdemarbeitsmarkt@users.noreply.github.com>
2024-08-01 20:35:29 -0700

    add placholder routes for registry & info routes; add placeholder templates as well

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,15 @@ from flask import flash, redirect, render_template, url_for
 from app import app
 
 
-@app.route('/', methods=['GET'])
-@app.route('/index', methods=['GET'])
+@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html', title='Colleen & Tim')
+
+@app.route('/registry')
+def registry():
+    return
+
+@app.route('/info')
+def info():
+    return
